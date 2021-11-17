05f96bafedca2de2095f07e6fa293e8f925bae55
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-16 19:22:38 -0800

    [Swetha] add : Contact details will be added

---
+++ AddressBook/AddressBook/AddContact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddContact
+    {
+        List<Contact> addressbook = new List<Contact>();
+       
+        public void AddConctact(Contact contact)
+        {
+            addressbook.Add(contact);
+        }
+        public void Display()
+        {
+            foreach(var contact in addressbook)
+            {
+                Console.WriteLine(contact.firstName+""+contact.lastName+""+contact.address+""+contact.city+""+contact.state+""+contact.zip+""+contact.phoneNumber+""+contact.email);
+               
+
+
+            }
+        }
+    }
+}
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -6,17 +6,18 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
+            Contact contact = new Contact();
             bool flag = true;
             while (flag)
             {
                 Console.WriteLine("Welcome to Address Book");
-                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Exit");
+                Console.WriteLine("Enter what you want to do : 1. Create Contacts ,2.Add Contact,3. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        Console.WriteLine("Enter the Contact details in specified format");
-                        Contact contact = new Contact()
+                        Console.WriteLine("Enter the Contact details");
+                        Contact newContact = new Contact()
                         {
                             firstName = Console.ReadLine(),
                             lastName = Console.ReadLine(),
@@ -29,6 +30,20 @@ namespace AddressBook
                         };
                         break;
                     case 2:
+                        Console.WriteLine("Add Contact details");
+                        contact.firstName = Console.ReadLine();
+                        contact.lastName = Console.ReadLine();
+                        contact.address = Console.ReadLine();
+                        contact.city = Console.ReadLine();
+                        contact.state = Console.ReadLine();
+                        contact.zip = Console.ReadLine();
+                        contact.phoneNumber = Console.ReadLine();
+                        contact.email = Console.ReadLine();
+                        AddContact add = new AddContact();
+                        add.AddConctact(contact);
+                        add.Display();
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
